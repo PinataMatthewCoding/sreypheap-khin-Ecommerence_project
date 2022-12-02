240a68c2d1faed15b6819c7df3bf69b9c8c186c7
SREYPHEAP.KHIN <sreypheap.khin@student.passerellesnumeriques.org>
2022-12-02 18:51:03 +0700

    views

---
+++ views/js/main.js
@@ -0,0 +1,66 @@
+function createCard(title, price, qty, col, img){
+    const card = document.createElement("div");
+    card.classList.add("card")
+    
+    const titleElement = document.createElement("div");
+    titleElement.classList.add("title");
+    titleElement.textContent = title;
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const priceElement = document.createElement("div");
+    priceElement.classList.add("price");
+    priceElement.textContent = "$" + price;
+
+    const qtyElement = document.createElement("div");
+    qtyElement.classList.add("qty");
+    qtyElement.textContent = qty;
+
+    const colElement = document.createElement("div");
+    colElement.classList.add("col");
+    colElement.textContent = col;
+
+    const imgElement = document.createElement("img");
+    // imgElement.classList.add("img");
+    imgElement.src = img;
+
+    cardBody.appendChild(priceElement);
+    cardBody.appendChild(qtyElement);
+    cardBody.appendChild(colElement);
+    cardBody.appendChild(imgElement);
+
+    card.appendChild(titleElement);
+    card.appendChild(cardBody);
+ 
+    return card;
+}
+
+//----------------------------------------display products on customer------------------------------//
+function displayProduct() {
+    let products = JSON.parse(localStorage.getItem("product-name")) ?? [];
+    for (let product of products) {
+        let card = createCard(product.name, product.price, product.quantity, product.color, product.Img);
+        container.appendChild(card);
+    }
+}
+const container = document.querySelector("#container");
+document.addEventListener("DOMContentLoaded", () => { displayProduct() });
+
+
+// ------------------------------------Customer search productslist----------------------------//
+function searchTask() {
+    let text = search.value.toLowerCase();
+    let tasks = document.querySelectorAll('.card');
+    for (let task of tasks) {
+      let taskTitle = task.firstElementChild.textContent.toLowerCase();
+      if (taskTitle.indexOf(text) === -1) {
+        task.style.display = "none";
+      } else {
+        task.style.display = "flex";
+      }
+    }
+}
+const search = document.getElementById("search");
+search.addEventListener('keyup', searchTask);
+console.log(search)
---
+++ views/seller/js/seller.js
@@ -0,0 +1,142 @@
+function addProductToLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function getProductFromLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key)) ?? [];
+}
+
+function createNewRecord(name, price, quantity, color, img, edit_delete) {
+    const tr = document.createElement("tr");
+    // tr.dataset.index =index
+    const tdOne = document.createElement("td");
+    const tdTwo = document.createElement("td");
+    const tdThree = document.createElement("td");
+    const tdFour = document.createElement("td");
+    const tdFive = document.createElement("td");
+    const tdSix = document.createElement("td");
+    tdOne.textContent = name;
+    tdTwo.textContent = price;
+    tdThree.textContent = quantity;
+    tdFour.textContent = color;
+    tdFive.textContent = img;
+    tdSix.textContent = edit_delete;
+
+    // ----------------Include img Edit and Delete in edit_delete---------------//
+    const buttonEdit = document.createElement("img");
+    buttonEdit.src = "img/edite.png"
+    
+    const buttonDelete = document.createElement("img");
+    buttonDelete.src = "img/delete.png"
+
+    tdSix.appendChild(buttonEdit);
+    tdSix.appendChild(buttonDelete);
+
+    //------------------------------For delete products ------------------------//
+    buttonDelete.addEventListener("click", (e) =>{
+        let index = e.target.parentElement.parentElement.dataset.index;
+    console.log(index)
+        productList.splice(index, 1);
+        addProductToLocalStorage("product-name", JSON.stringify(productList));
+        displayProduct();
+    })
+
+    //-----------------------------for edit products---------------------------//
+    // buttonEdit.className = "edit"
+    // buttonEdit.src = "img/edite.png";
+
+    buttonEdit.addEventListener('click',(e)=>{
+        let index = e.target.parentElement.parentElement.dataset.index;
+    
+        document.getElementById("product-name").value = name;
+        document.getElementById("product-price").value = price;
+        document.getElementById("product-quantity").value = quantity;
+        document.getElementById("product-color").value= color;
+        document.getElementById("product-img").value = img;
+  
+    })
+
+    tr.appendChild(tdOne);
+    tr.appendChild(tdTwo);
+    tr.appendChild(tdThree);
+    tr.appendChild(tdFour);
+    tr.appendChild(tdFive);
+    tr.appendChild(tdSix);
+
+    return tr;
+}
+
+function createTableHeader() {
+    const headerRow = document.createElement("tr");
+    const thOne = document.createElement("th");
+    const thTwo = document.createElement("th");
+    const thThree = document.createElement("th");
+    const thFour = document.createElement("th")
+    const thFive = document.createElement("th")
+    const thSix = document.createElement("th");
+    
+    thOne.textContent = "name"
+    thTwo.textContent = "price";
+    thThree.textContent = "quantity";
+    thFour.textContent = "color"
+    thFive.textContent = "img"
+    thSix.textContent = "edit_delete"
+
+    headerRow.appendChild(thOne);
+    headerRow.appendChild(thTwo);
+    headerRow.appendChild(thThree);
+    headerRow.appendChild(thFour);
+    headerRow.appendChild(thFive);
+    headerRow.appendChild(thSix);
+
+    return headerRow;
+}
+// -------------------------function display on seller -----------------//
+function displayProduct() {
+    if(tableData.firstElementChild !== null ) {
+        document.querySelector("table").remove();
+    }
+    const  newTable = document.createElement("table");
+    newTable.appendChild(createTableHeader());
+    let products = getProductFromLocalStorage("product-name");
+    for (let product of products) {
+        let row = createNewRecord(product.name, product.price, product.quantity, product.color, product.Img);
+        newTable.appendChild(row)
+    }
+    tableData.appendChild(newTable);
+}
+
+const result = document.querySelector("#result");
+const productName = document.querySelector("#product-name");
+const productPrice = document.querySelector("#product-price");
+const productQuantity = document.querySelector("#product-quantity");
+const productColor = document.querySelector("#product-color");
+const productImg = document.querySelector("#product-img");
+const button = document.querySelector("button");
+const tableData = document.querySelector(".table-data");
+
+let productList = JSON.parse(localStorage.getItem("product-name")) ?? [];
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (productName.value === "") {
+        return;
+    }
+    let productObject = {name: productName.value, price: productPrice.value, quantity: productQuantity.value, color: productColor.value, Img: productImg.value}
+
+    productList.push(productObject);
+
+    productName.value = ""
+    productPrice.value = ""
+    productQuantity.value = ""
+    productColor.value = ""
+    productImg.value = ""
+    // add the product
+    addProductToLocalStorage("product-name", JSON.stringify(productList));
+
+    displayProduct();
+})
+
+document.addEventListener("DOMContentLoaded", () => { displayProduct() })
+
+
